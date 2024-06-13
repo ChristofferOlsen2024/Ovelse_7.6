27be9fb5abd74f8ea61df122e12e5966233bfe53
ChristofferOlsen2024 <ch.o@mailfence.com>
2024-06-13 08:32:14 +0200

    Svær færdig :D

---
+++ Ovelse_7.6/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ovelse_7._6
+{
+    internal class Person
+    {
+
+        public required String Navn { get; set; }
+        public required String Email { get; set; }
+
+        public void Udskriv()
+        {
+            Console.WriteLine($"Personen hedder {Navn} og email er {Email}");
+        }
+    }
+}
